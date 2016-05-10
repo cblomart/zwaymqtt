6d1513c141afae0299a69e5de783838764d87298
BLOMART CEDRIC J.F <cedric.blomart@minfin.fed.be>
2016-05-11 00:21:44 +0200

    Setpoint value as int (val not setval)

--- zwaymqtt.go
+++ zwaymqtt.go
@@ -724,7 +724,7 @@ func zwayparsedevices(update map[string]interface{}) {
                 zway_home, normName(givenName), i, normName(setpointType),
                 normName(setpointScale))
               gateways = append(gateways, Gateway{Key: nkey, Topic: topic,
-                 Value: "setVal.value", Write:true, Type: "float"})
+                 Value: "val.value", Write:true, Type: "int"})
             }
           }
         }
